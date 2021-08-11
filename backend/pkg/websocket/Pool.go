3a4b2fb0485277ce4c7a11570a11e6d626fbfd5f
Azan <azanulhaque@gmail.com>
2021-08-11 21:43:07 +0530

    Pool created

---
+++ backend/pkg/websocket/Pool.go
@@ -0,0 +1,17 @@
+package websocket
+
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}

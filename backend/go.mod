47cd47be9f0c95629cdef8fb4912a1e88065c74a
Azan <azanulhaque@gmail.com>
2021-08-11 11:12:30 +0530

    api/index.js created

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module githib.com/Azanul/sigret-chat-app
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+func reader(conn *websocket.Conn){
+	for{
+		msgType, p, err := conn.ReadMessage()
+		if err!=nil{
+			fmt.Println(err)
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(msgType, p); err != nil{
+			log.Println(err)
+			return
+		}
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func servews(w http.ResponseWriter, r* http.Request)  {
+	fmt.Println(r.Host)
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil{
+		fmt.Println(err)
+	}
+	reader(ws)
+}
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	http.HandleFunc("/ws", servews)
+}
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }

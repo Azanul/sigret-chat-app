d118c8c29d9a35dd687505a31c6575a70f48a28d
Azan <azanulhaque@gmail.com>
2021-08-11 13:47:06 +0530

    Header created and added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {Component} from "react";
 import {connect, sendMsg} from "./api";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class App extends Component {
   render() {
     return (
         <div className="App">
+          <Header/>
           <button onClick={this.send}>Hit</button>
         </div>
     );
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.scss';
+import React, {Component} from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h2>Chat-App</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
